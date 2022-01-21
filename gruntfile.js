9b3f4a09f8907a94f182752ddf6b56a47aec1003
Umesh Patil <pumesh@zeomega.com>
2022-01-21 18:20:00 +0530

    grunt commit

---
+++ gruntfile.js
@@ -0,0 +1,106 @@
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        autoprefixer: {
+            options: {
+                //write task specific options
+                browsers: ['last 2 versions', 'firefox>3']
+            },
+            your_target: {
+                //target options
+            }
+        },
+        watch: {
+            files: ['css/style.css'],
+            tasks: ['autoprefixer']
+        },
+        copy: {
+            src: 'css/style.css',
+            dest: 'css/styleprefixed.css'
+        },
+        cssmin: {
+            target: {
+                files: {
+                    'css/styleprefixed.min.css': ['css/styleprefixed.css']
+                }
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'js/output.min.js': ['js/*.js']
+                }
+            }
+        },
+        imagemin: {
+            files: {
+                expand: true,
+                cwd: 'images/',
+                src: ['**/*.{jpg,png,gif}'],
+                dest: 'images/build'
+            }
+        },
+        jshint: {
+            options: {
+                "bitwise": true,
+                "camelcase": true,
+                "latedef": true,
+                "newcap": true,
+                "nonew": true,
+                "undef": true,
+                "esnext": true,
+                "sub": true,
+                "browser": true,
+                "node": true,
+                "jquery": true,
+                "devel": true
+            },
+            target: {
+                src: ['js/**/*.js', '!js/output.min.js']
+            }
+        },
+        concurrent:{
+            target:{
+                tasks:['watch','autoprefixer','imagemin'],
+                options:{
+                    logConcurrentOutput:true
+                }
+            }
+        },
+        qunit:{
+            all:{
+                options:{
+                    urls:['http://localhost:8000']
+                }
+            }
+        },
+        connect:{
+            server:{
+                options:{
+                    port:8000,
+                    base:'tests/',
+                    keepalive:true
+                }
+            }
+        },
+        compress:{
+            main:{
+                options:{
+                    archive:'site.zip'
+                },
+                files:[
+                    {expand:true,src:['css/*'],dest:'/'}
+                ]
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.registerTask("default", ['autoprefixer']);
+    grunt.registerTask("minifyNewImages", ['newer:imagemin']);
+    grunt.registerTask("build", ['cssmin','uglify','imagemin','compress']);
+};
